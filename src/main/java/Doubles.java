9558ce40198a8ff091425936d6ac2d0ec67b0aeb
inspiredjava <wh_sh@mail.ru>
2016-03-24 19:55:05 +0200

    decimal part

--- src/main/java/Doubles.java
+++ src/main/java/Doubles.java
@@ -22,7 +22,7 @@ public class Doubles {
         }
 
         public Double getResult() {
-            if (currentState == State.NUMBER) return new Double(parseData.getNumber());
+            if (currentState == State.NUMBER || currentState == State.DECIMAL_NUMBER) return new Double(parseData.getNumber());
             return null;
         }
 
@@ -37,7 +37,7 @@ public class Doubles {
 
                     int digit = c - '0';
                     if (digit <= 9 && digit >= 0) {
-                        parseData.addDigit(digit);
+                        parseData.addIntPart(digit);
                         return NUMBER;
                     }
                     return INVALID_END;
@@ -47,8 +47,13 @@ public class Doubles {
                 public State next(char c, ParseData parseData) {
                     int digit = c - '0';
                     if (digit <= 9 && digit >= 0) {
-                        parseData.addDigit(digit);
+                        parseData.addIntPart(digit);
                         return NUMBER;
+                    }
+
+                    if(c =='.') {
+//                        parseData.addDecimalPart();
+                        return DECIMAL_NUMBER;
                     }
                     return INVALID_END;
                 }
@@ -63,27 +68,49 @@ public class Doubles {
                 public State next(char c, ParseData parseData) {
                     return INVALID_END;
                 }
+            }, DECIMAL_NUMBER {
+                @Override
+                public State next(char c, ParseData parseData) {
+                    if (c == '.') {
+                        return INVALID_END;
+                    }
+
+                    int digit = c - '0';
+                    if (digit <= 9 && digit >= 0) {
+                        parseData.addDecimalPart(digit);
+                        return DECIMAL_NUMBER;
+                    }
+
+                    return INVALID_END;
+                }
             };
 
             public abstract State next(char c, ParseData parseData);
         }
 
         public static class ParseData {
-            private int number;
+            private double number;
+            private double decimalPart;
+            private int decCount = 1;
             private int sign = 1;
 
-            public void addDigit(int digit) {
+            public void addIntPart(int digit) {
                 number = number * 10 + digit;
             }
 
-            public int getNumber() {
-                return number * sign;
+            public double getNumber() {
+                return number * sign + decimalPart;
             }
 
             public void setSign(char sign) {
                 if (sign == '-') {
                     this.sign = -1;
                 }
+            }
+
+            public void addDecimalPart(int digit) {
+                decCount *= 10;
+                decimalPart = decimalPart + (double) digit/decCount;
             }
         }
     }
